a805b808edf71af3741025cec65b50a4a1b892cd
Ehsan Mohammadpour <ehmohamm@icsil1-210413.local>
2021-04-23 15:19:01 +0200

    performance of add function is added

--- data structures/list/list-add-performance.py
+++ data structures/list/list-add-performance.py
@@ -1,22 +1,31 @@
 import time
+from functools import reduce
 
 
 list_ones = [1] * 10000
 
 t_start = time.time_ns()
-# start of the code
+# start of method 1
 builtin_sum = sum(list_ones)
-# end of the code
+# end of method 1
 t_end = time.time_ns()
 time_elapsed = t_end-t_start
 print('Built-in method -> result of sum: %d,  Elapsed time: %d ns'%(builtin_sum,time_elapsed))
 
 t_start = time.time_ns()
-# start of the code
+# start of method 2
 iter_sum=0
 for i in list_ones:
     iter_sum += i
-# end of the code
+# end of method 2
 t_end = time.time_ns()
 time_elapsed = t_end-t_start
 print('For-loop method -> result of sum: %d,  Elapsed time: %d ns'%(iter_sum,time_elapsed))
+
+t_start = time.time_ns()
+# start of method 3
+reduce_sum = reduce(lambda x,y: x+y,list_ones)
+# end of method 3
+t_end = time.time_ns()
+time_elapsed = t_end-t_start
+print('Reduce method -> result of sum: %d,  Elapsed time: %d ns'%(reduce_sum,time_elapsed))
